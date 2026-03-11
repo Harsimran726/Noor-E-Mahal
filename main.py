0f397319afad14eb9a11ba251c2881e59e3fe26a
Harsimran Singh <harsimransd726@gmail.com>
2026-03-11 10:14:22 +0530

    update vercel.json

--- main.py
+++ main.py
@@ -243,5 +243,5 @@ async def upload_image(
     return {"status": "success", "url": relative_path}
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
